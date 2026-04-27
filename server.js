bc196e2a3f4ae46cf066e9f47aeaf359f553e44f
o-anvarbekov15 <omadbekanvarbekov00@gmail.com>
2026-04-27 18:29:31 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post("/create-item", (req, res) => {
 });
 
 app.get("/", function(req, res) {
-    res.render('harid');
+    res.render('reja');
 });
 
 app.get('/author', (req, res) => {
@@ -48,5 +48,5 @@ app.get('/author', (req, res) => {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT,  function () {
-  console.log(`The server is running successfully on port: ${PORT}`);
+  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
